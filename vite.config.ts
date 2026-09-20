8cba89a17ab3a9edcfd909a46e408dae92a66fe7
bartomeuagusti-prog <bartomeuagusti-prog@users.noreply.github.com>
2026-09-20 12:16:42 +0200

    Configure base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/benlligat-landing/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
